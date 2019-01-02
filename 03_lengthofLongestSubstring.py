cba41a65f616c5b3a827702b97ced38907499a12
unknown <kaseyriver11@gmail.com>
2019-01-01 20:28:15 -0500

    Adding solution for length of longest subtring.

--- 03_lengthofLongestSubstring.py
+++ 03_lengthofLongestSubstring.py
@@ -18,13 +18,81 @@ Explanation: The answer is "wke", with the length of 3.
 
 """
 
+# ----- Solution #1
 class Solution:
     def lengthOfLongestSubstring(self, s):
         """
         :type s: str
         :rtype: int
         """
+        if len(s) > 0:
+            d = {}
+            max_d = {}
+            for i, letter in enumerate(s):
+                val = s[(i + 1):].find(letter)
+                if val == -1:
+                    d[i] = len(s)
+                elif val == 0:
+                    d[i] = i + 1
+                else:
+                    d[i] = val + i + 1
 
-        return 'hi'
+            for i in d:
+                max_d[i] = d[i] - i
 
-s = 'randomstringabcranstr'
+            for item in d:
+                for i in range(item + 1, d[item]):
+                    if d[item] > d[i]:
+                        max_d[item] = min(d[i] - item, max_d[item])
+            return max([max(max_d.values()), 1])
+        return 0
+
+
+# ----- Solution #2:
+class Solution:
+    def lengthOfLongestSubstring(self, string):
+        """
+        Time:  O(n)
+        Space: O(k)
+        [k = length of the longest substring w/o repeating characters]
+        """
+        longest = 0
+        left, right = 0, 0
+        chars = set()
+        while left < len(string) and right < len(string):
+            if string[right] not in chars:
+                chars.add(string[right])
+                right += 1
+                longest = max(longest, right - left)
+            else:
+                chars.remove(string[left])
+                left += 1
+        return longest
+
+
+# ----- Solution #3:
+class Solution:
+    def lengthOfLongestSubstring(self, string):
+        max_d = 0
+        left_place, right_place = 0, 0
+        letters = set()
+
+        while right_place < len(string):
+            if string[right_place] not in letters:
+                letters.add(string[right_place])
+                right_place += 1
+                max_d = max(max_d, right_place - left_place)
+            else:
+                letters.remove(string[left_place])
+                left_place += 1
+
+        return max_d
+
+
+Solution().lengthOfLongestSubstring("cdd")
+Solution().lengthOfLongestSubstring("abcabcbb")
+Solution().lengthOfLongestSubstring("au")
+Solution().lengthOfLongestSubstring(" ")
+Solution().lengthOfLongestSubstring("jlygy")
+Solution().lengthOfLongestSubstring("uqinntq")
+
