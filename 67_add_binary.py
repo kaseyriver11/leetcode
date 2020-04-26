25c553a1ad19eaa58de4c6c226405b0cb137d1d9
unknown <kaseyriver11@gmail.com>
2020-04-26 19:37:59 -0400

    Adding 67 question and solution.

---
+++ 67_add_binary.py
@@ -0,0 +1,9 @@
+"""
+Given two binary strings, return their sum (also a binary string).
+
+The input strings are both non-empty and contains only characters 1 or 0.
+"""
+
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        return "{0:b}".format((int(a, 2) + int(b, 2)))
