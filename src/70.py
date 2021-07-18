f9fe765d63305c58abe303704a9eac5cf7b2a3a3
Kasey Jones <krjones@rti.org>
2021-07-18 12:10:44 -0400

    Adding code for problem 70.

---
+++ src/70.py
@@ -0,0 +1,82 @@
+"""
+You are climbing a staircase. It takes n steps to reach the top.
+
+Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+
+Example 1:
+
+Input: n = 2
+Output: 2
+Explanation: There are two ways to climb to the top.
+1. 1 step + 1 step
+2. 2 steps
+Example 2:
+
+Input: n = 3
+Output: 3
+Explanation: There are three ways to climb to the top.
+1. 1 step + 1 step + 1 step
+2. 1 step + 2 steps
+3. 2 steps + 1 step
+"""
+
+class Solution:
+    @staticmethod
+    def climbStairs(self, n: int) -> int:
+        if n < 4:
+            return n
+        count0 = 2
+        count1 = 3
+        n2 = 4
+        while n2 != n:
+            new_sum = count0 + count1
+            count0 = count1
+            count1 = new_sum
+        return new_sum
+        
+        
+
+Solution.climbStairs(1)
+
+# 2: 2
+
+1 1
+2
+
+# 3: 3
+1 1 1
+1 2 
+2 1
+
+# 4: 5
+1 1 1 1
+1 1 2
+1 2 1
+2 1 1
+2 2
+
+# 5: 8
+1 1 1 1 1
+1 1 1 2
+1 1 2 1
+1 2 1 1
+2 1 1 1
+1 2 2
+2 1 2
+2 2 1
+
+
+# 6: 13
+1 1 1 1 1 1
+1 1 1 1 2
+1 1 1 2 1
+1 1 2 1 1
+1 2 1 1 1
+2 1 1 1 1 
+1 1 2 2 
+1 2 1 2
+1 2 2 1
+2 1 1 2
+2 1 2 1
+2 2 1 1
+2 2 2
