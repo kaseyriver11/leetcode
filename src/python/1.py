aa032eadfcd4258b2774abf22188180fecd627e1
Kasey Jones <krjones@rti.org>
2021-07-20 08:02:56 -0400

    Reworking python solution for question 1. Adding explanation of solution

--- src/python/1.py
+++ src/python/1.py
@@ -9,27 +9,22 @@ Because nums[0] + nums[1] = 2 + 7 = 9,
 return [0, 1].
 """
 
-
 class Solution:
     @staticmethod
-    def twoSum(nums, target):
+    def twoSum(nums: list, target: int) -> list:
         """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
+        This problem tests the use of a map. We want to store the index of the matching value.
+        For example, if the first number is 2 and the target is 9, the value we want to is 7.
+        Store the key 7, with the index corresponding to 2. 
         """
-        for i, value in enumerate(nums):
-            for j, value2 in enumerate(nums):
-                if value + value2 == target:
-                    if i != j:
-                        return [i, j]
+        target_dict = dict()
 
+        for index, number in enumerate(nums):
+            if number in target_dict:
+                return [target_dict[number], index]
+            target_dict[target - number] = index
 
-class Solution:
-    @staticmethod
-    def twoSum(nums, target):
-        c_map = {}
-        for i, value in enumerate(nums):
-            if value in c_map:
-                return [c_map[value], i]
-            c_map[target - value] = i
+# ----- Test
+nums = [2, 7, 11, 15]
+target = 9
+Solution.twoSum(nums, target)
